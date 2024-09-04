8ea31226af769b33e7a34cf8052991d6760bf48c
Petr Breit <104927244+PanUcitelCz@users.noreply.github.com>
2024-09-04 14:56:59 +0000

    Oprava

--- nasobilka.py
+++ nasobilka.py
@@ -1,20 +1,25 @@
-import random #Importujeme knihovnu s funkcema pro náhodné generování čísel
+import random  # Importujeme knihovnu s funkcemi pro náhodné generování čísel.
 
 def nasobeni(a, b):
+    # Funkce, která násobí dvě čísla a vrací výsledek.
     vysledek = a * b
-    return vysledek
+    return vysledek  # Vrací výsledek násobení.
 
 def vyhodnoceni(vysledek, porovnani):
-    odpoved = False
-    if vysledek == porovnani:
-        print("Ty jsi ale šikulka, máš správně")
-        odpoved = True
+    # Funkce, která vyhodnocuje, zda se zadaný výsledek shoduje s výsledkem násobení.
+    odpoved = False  # Přednastavíme odpověď jako False (nesprávně).
+    if vysledek == porovnani:  # Pokud výsledek odpovídá zadané hodnotě:
+        print("Ty jsi ale šikulka, máš správně")  # Uživatel dostane pochvalu.
+        odpoved = True  # Odpověď je True (správně).
     else:
-        print("Ahh, zrovna jsi se spletl, příště to bude lepší")
-    return odpoved
+        print("Ahh, zrovna jsi se spletl, příště to bude lepší")  # Pokud je špatně, vytiskne se povzbuzení.
+    return odpoved  # Vrací, zda byla odpověď správná (True) nebo ne (False).
 
-x = random.randint(1,10)
-y = random.randint(1,10)
+x = random.randint(1, 10)  
+# Vygeneruje náhodné číslo od 1 do 10 a přiřadí ho k proměnné x.
 
+y = random.randint(1, 10)  
+# Vygeneruje náhodné číslo od 1 do 10 a přiřadí ho k proměnné y.
 
-porovnani = input(f"{x} * {y} = ")
+porovnani = input(f"{x} * {y} = ")  
+# Zeptá se uživatele na výsledek násobení dvou čísel x a y a uloží ho do proměnné porovnani.
